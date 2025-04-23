8c8ee0407711c553d6aa57a2e0705d6a64f28432
sentidas <sentidas@yandex.ru>
2025-04-23 07:55:17 +0300

    feat: add DAO and model classes: UserdataUserDAO, UserdataUserDAOJdbc, UserEntity, UserJson

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/UserdataUserDAO.java
@@ -0,0 +1,19 @@
+package guru.qa.niffler.data.dao;
+
+import guru.qa.niffler.data.entity.userdata.UserEntity;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserdataUserDAO {
+
+    UserEntity createUser(UserEntity user);
+
+    Optional<UserEntity> findById(UUID id);
+
+    Optional<UserEntity> findByUsername(String username);
+
+    void deleteUser(UserEntity user);
+
+
+}
